e1acc2739b15427c7d1c31c0ef886b83be9e6367
ty要nx <ttyy373172968@163.com>
2021-04-27 21:56:03 +0800

    完成分类参数基本页面的一部分

---
+++ src/components/goods/Params.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>参数列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <!-- 头部的警告区域 -->
+      <el-alert title="注意：只允许" type="warning" :closable="false" show-icon>
+      </el-alert>
+      <!-- 选择商品分类区域 -->
+      <el-row class="cat_opt">
+        <el-col>
+          <span>选择商品分类：</span>
+          <!-- 选择商品分类的级联选择器 -->
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  created() {},
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+.cat_opt {
+  margin: 15px 0;
+}
+</style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,3 @@
-
 import Vue from 'vue'
 import {
   Button,
@@ -29,7 +28,8 @@ import {
   Tree,
   Select,
   Option,
-  Cascader
+  Cascader,
+  Alert
 } from 'element-ui'
 
 Vue.use(Button)
@@ -59,5 +59,6 @@ Vue.use(Tree)
 Vue.use(Select)
 Vue.use(Option)
 Vue.use(Cascader)
+Vue.use(Alert)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
